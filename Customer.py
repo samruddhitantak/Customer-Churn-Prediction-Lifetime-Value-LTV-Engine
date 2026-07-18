aada2a13bcc565c811aee09693c3cc22a3dfdd1e
samruddhitantak <samruddhitantak142@gmail.com>
2026-07-19 00:00:02 +0530

    Finalize a Project

--- Customer.py
+++ Customer.py
@@ -6,7 +6,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
-
 # 1. Load Dataset
 try:
     df = pd.read_csv("Customer Churn Prediction & Lifetime Value (LTV) Engine.csv")
